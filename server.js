95e92b1075c47b634a7929b16857440146f43ee3
unknown <lakshmimadhavi37@gmail.com>
2025-03-19 11:09:07 +0530

    Connected to mongodb

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
+// Import the Express module
 const express = require('express');
 const app = express();
-const port = 3000;
+const mongoose = require('mongoose');
+app.use(express.json())
+require('dotenv').config()
+const PORT = 3000;
 
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(()=>{
+    console.log("mongodb is connected");
+})
+.catch((error)=>{
+    console.log("Failed to load",error);
+})
+
+// Root route
+app.get('/', (req, res) => {
+    res.send('Welcome to the Express server!');
+});
+                                          
+// /ping route
 app.get('/ping', (req, res) => {
-  res.send('pong');
+    res.send('pong');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
